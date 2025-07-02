229278ed7c1ba088f3a3b20c7a86da8b9a9187f0
Cornelius-Kisoi <142579567+Cornelius-Kisoi@users.noreply.github.com>
2025-07-02 21:22:09 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 #app = Flask(__name__) # Create a Flask WSGI application, an instance of the Flask class.
 
 @app.route('/')
 def hello_world():
-  return 'Hello World'
+  return render_template('index.html')
   #return 'Hello World!'
 
 if __name__ == '__main__':
